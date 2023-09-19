bc49ab17b99443f866aac106be116f061328a712
Frandorr1 <142456182+Frandorr1@users.noreply.github.com>
2023-09-19 19:29:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,24 @@ console.log(numero)
 //a) O que vai ser impresso no console? IMPRESSO NA VERTICAL 19, 21, 23, 25, 27, 30.
 
 //b) Se eu quisesse acessar o índice de cada elemento dessa lista, o for...of... é suficiente? Se sim, o que poderia ser usado para fazer isso?
+//A sintaxe "for...of" por si só não permite acessar o índice dos elementos da lista diretamente. Para acessar o índice,pode usar uma nova variável ou combinar os dois
+//"for...of" e "for...if"com um array para acessar o índice de cada elemento. Portanto,a sintaxe "for...of" é suficiente para acessar o índice.
+//ex:
+const lista = [ 10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30 ]
+
+for (let numero of lista) {
+	if (numero > 18) {
+		console.log(numero)
+	}
+}
 
 
 
 //3. Qual seria o resultado impresso no console, se o usuário digitasse o número 4 ?
+//*
+//**
+//***
+//****
 
 const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
 let quantidadeAtual = 0
@@ -41,10 +55,9 @@ linha += "*"
 console.log(linha)
 quantidadeAtual++
 }
-//*
-//**
-//***
-//****
+
+
+
 
 
 
